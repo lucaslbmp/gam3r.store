bb61684b7708b8025fc53731a59890525b2343e1
Lucas <lucaslbmp@github.com>
2024-12-06 20:43:51 -0300

    fix: changes to deploy as a monorepo

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { ProdutoModule } from './produto/produto.module';
 import { PedidoModule } from './pedido/pedido.module';
 import { DbModule } from './db/db.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ProdutoModule, PedidoModule, DbModule],
+  imports: [
+    ProdutoModule,
+    PedidoModule,
+    DbModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'frontend', '.next'),
+      exclude: ['backend/*'],
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
